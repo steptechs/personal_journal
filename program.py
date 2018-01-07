6f733057fd3fde5557fe5e26284dcf9bd07c8de0
steptechs <35179431+steptechs@users.noreply.github.com>
2018-01-07 20:30:51 +0000

    Sketched outline

---
+++ program.py
@@ -0,0 +1,16 @@
+def print_header():
+    print("-------------------------------")
+    print("        JOURNAL APP")
+    print("-------------------------------")
+
+
+def run_event_loop():
+    pass
+
+
+def main():
+    print_header()
+    run_event_loop()
+
+
+main()
